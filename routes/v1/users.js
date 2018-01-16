c959ed3c9431af1e2eb45e94c5a9b6583d6551ba
branislavantic87 <branislav.antic@cadev.rs>
2018-01-16 12:28:40 +0100

    gotov user api

--- routes/v1/users.js
+++ routes/v1/users.js
@@ -3,12 +3,68 @@ var router = express.Router();
 var User = require('../../models/user');
 var m = require('../../middlewares/middleware');
 
+// INDEX
 router.get('/', (req, res) => {
     User.find({}, (err, allUsers) => {
         if (err){
-            res.json(err)
+            res.sendStatus(404);
         } else {
             res.json(allUsers)
+        }
+    })
+})
+
+// SHOW
+router.get('/:id', (req, res) => {
+    User.findById(req.params.id).populate({path: 'courses', populate : {path: 'students.data'}}).exec ( (err, foundUser) => {
+        if(err || !foundUser){
+            res.status(404);
+        } else {
+            res.json(foundUser);
+        }
+    })
+})
+
+// EDIT
+router.get('/:id/edit', (req, res) => {
+    User.findById(req.params.id, (err, foundUser) => {
+        if(err || !foundUser){
+            res.status(404);
+        } else {
+            res.json(foundUser)
+        }
+    })
+})
+
+// Update 
+
+router.put('/:id', (req, res) => {
+    let newData = {
+        $set: {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email
+        }
+    };
+    req.body.image ==='' ? newData.$unset = {image: ''} : newData.$set.image = req.body.image;
+    User.findByIdAndUpdate(req.params.id, newData, (err, updatedUser) => {
+        if(err){
+            res.status(404);
+        } else {
+            res.status(200)
+        }
+    })
+})
+
+// DELETE
+
+router.delete('/:id', (req, res) => {
+    User.findById(req.params.id, (err, deleted) => {
+        if(err || !deleted){
+            res.status(404)
+        } else {
+            deleted.remove();
+            res.status(200).json({"Deleted": true})
         }
     })
 })
